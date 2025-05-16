0e97b4193330b6151913dec596d48e3edaa5e2fe
the-real-nox <the-real-nox@outlook.com>
2025-05-16 12:28:48 +0200

    Still testing. Found out that Spotify removed the audio-features api :(

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
+import spotipy
+from spotipy import SpotifyOAuth
+from dotenv import load_dotenv
+from time import sleep
+from json import dumps
 
 def main():
-    pass
-
+    load_dotenv()
+    
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+        redirect_uri="http://127.0.0.1:9999/spotipy",
+        scope="user-read-playback-state user-read-currently-playing"
+    ))
+    
+    current_id = None
+    bpm = None
+    name = None
+    
+    while True:
+        song = sp.current_playback()
+        
+        if current_id is None or song["item"]["id"] != current_id:
+            current_id = song["item"]["id"]
+            bpm = sp.audio_features(["3n3Ppam7vgaVa1iaRUc9Lp"])[0]
+            name = song["item"]["name"]
+        
+        
+        
+        print(f"{name}: {bpm}")
+        
+        sleep(1)
+    
+    
 if __name__ == '__main__' :
     main()
